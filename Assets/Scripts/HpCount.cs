65a50af347f6a9cd7cf7666dee93563c32834dcf
MishaPolar <70522622+MishaPolar@users.noreply.github.com>
2021-04-19 18:10:44 +0700

    unit

---
+++ Assets/Scripts/HpCount.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HpCount : MonoBehaviour
+{
+    [SerializeField] private float _VisualChangeSpeed = 3;
+    [SerializeField] private Slider HpBar;
+    [SerializeField] private int _maxHp = 100;
+
+    private int _hp;
+
+    private void Start()
+    {
+        _hp = _maxHp;
+        HpBar.value = _hp;
+        HpBar.maxValue = _maxHp;
+    }
+
+    public void ChangeHp(int changeValue)
+    {
+        _hp += changeValue;
+        if (_hp > _maxHp)
+            _hp = _maxHp;
+        else if (_hp < 0)
+            _hp = 0;
+    }
+
+    private void Update()
+    {
+        if (HpBar.value != _hp)
+            HpBar.value = Mathf.MoveTowards(HpBar.value, _hp, _VisualChangeSpeed);
+    }
+}
