6c050fde353feae31555121d1de33b5ee0c1e3cc
MishaPolar <70522622+MishaPolar@users.noreply.github.com>
2021-04-23 22:00:07 +0700

    rebuild System
    rebuild HpBar System

---
+++ Assets/Scripts/HpBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+    [SerializeField] private float _hpChangeSpeed = 1;
+    [SerializeField] private HpCount _hpCount;
+
+    private Slider _hpBar;
+
+    private void Start()
+    {
+        _hpBar = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if (_hpBar.value != _hpCount.Hp)
+            _hpBar.value = Mathf.MoveTowards(_hpBar.value, _hpCount.Hp, _hpChangeSpeed);
+    }
+}
--- Assets/Scripts/HpCount.cs
+++ Assets/Scripts/HpCount.cs
@@ -3,17 +3,14 @@ using UnityEngine.UI;
 
 public class HpCount : MonoBehaviour
 {
-    [SerializeField] private float _VisualChangeSpeed = 3;
-    [SerializeField] private Slider HpBar;
     [SerializeField] private int _maxHp = 100;
 
     private int _hp;
+    public int Hp { get => _hp;}
 
     private void Start()
     {
         _hp = _maxHp;
-        HpBar.value = _hp;
-        HpBar.maxValue = _maxHp;
     }
 
     public void ChangeHp(int changeValue)
@@ -23,11 +20,5 @@ public class HpCount : MonoBehaviour
             _hp = _maxHp;
         else if (_hp < 0)
             _hp = 0;
-    }
-
-    private void Update()
-    {
-        if (HpBar.value != _hp)
-            HpBar.value = Mathf.MoveTowards(HpBar.value, _hp, _VisualChangeSpeed);
     }
 }
