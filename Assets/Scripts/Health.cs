3cad37afec3e4aa55375d8cdac0909c229e98efd
MishaPolar <70522622+MishaPolar@users.noreply.github.com>
2021-05-16 02:54:55 +0700

    Global Scripts Update
    Script rewriting(corutine using)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
+
+    public event UnityAction<int> HealthChanged;
+
+    public int MaxHealth => _maxHealth;
+
+    private void Start()
+    {
+        HealthChanged?.Invoke(_health);
+    }
+
+    public void ApplyHealthChanging(int value)
+    {
+        int startHealth = _health;
+        if (_health + value < 0)
+        {
+            _health = 0;
+        }
+        else if (_health + value > _maxHealth)
+        {
+            _health = _maxHealth;
+        }
+        else
+        {
+            _health += value;
+        }
+
+        if (startHealth != _health)
+            HealthChanged?.Invoke(_health);
+    }
+
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private float _ChangeSpeed;
+    [SerializeField] private Health _health;
+
+    private int _targetHealth;
+    private bool IsHealthChanging = false;
+
+    private Slider _healthSlider;
+
+    private void Start()
+    {
+        _healthSlider = GetComponent<Slider>();
+        _healthSlider.maxValue = _health.MaxHealth;
+    }
+
+    private void OnEnable()
+    {
+        _health.HealthChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        _health.HealthChanged -= OnHealthChanged;
+    }
+
+    private void OnHealthChanged(int value)
+    {
+        _targetHealth = value;
+
+        if (IsHealthChanging == false)
+            StartCoroutine(ChangingHealth());
+
+    }
+
+    private IEnumerator ChangingHealth()
+    {
+        IsHealthChanging = true;
+
+        while(_healthSlider.value != _targetHealth)
+        {
+            float healthDelta = _ChangeSpeed * Time.deltaTime;
+            _healthSlider.value = Mathf.MoveTowards(_healthSlider.value, _targetHealth, healthDelta);
+            yield return null;
+        }
+
+        IsHealthChanging = false;
+    }
+
+}
